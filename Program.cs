6f562d8ab928a935dc05d770224893736a0fa0ee
FilipeRichterSena <filipe.richter.sena@gmail.com>
2022-12-29 10:24:41 -0300

    Função Divisão

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Calculadora
     {
         static void Main(string[] args)
         {
-            Subtracao();
+            Divisao();
         }
 
         static void Soma() 
@@ -44,6 +44,24 @@ namespace Calculadora
 
             float resultado = v1-v2;
             Console.WriteLine($"O resultado da subtração é {resultado}");
+
+            Console.ReadKey();
+        }
+    
+        static void Divisao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor:");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = v1/v2;
+            Console.WriteLine($"O resultado da divisão é {resultado}");
 
             Console.ReadKey();
         }
