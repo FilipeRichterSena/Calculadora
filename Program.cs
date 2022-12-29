dfa493084186bb2a24b1e959815cc088035c25ce
FilipeRichterSena <filipe.richter.sena@gmail.com>
2022-12-29 09:58:16 -0300

    Função Subtração

--- Program.cs
+++ Program.cs
@@ -6,13 +6,13 @@ namespace Calculadora
     {
         static void Main(string[] args)
         {
-            Soma();
+            Subtracao();
         }
 
         static void Soma() 
         {
             Console.Clear();
-            
+
             Console.WriteLine("Primeiro valor: ");
             float v1 = float.Parse(Console.ReadLine());
 
@@ -26,6 +26,26 @@ namespace Calculadora
             Console.WriteLine($"O resultado da soma é {resultado}");
             // Console.WriteLine($"O resultado da soma é {v1 + v2}");
             // Console.WriteLine($"O resultado da soma é " + (v1 + v2));
+
+            Console.ReadKey();
+        }
+
+        static void Subtracao() 
+        {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = v1-v2;
+            Console.WriteLine($"O resultado da subtração é {resultado}");
+
+            Console.ReadKey();
         }
     }
 }
